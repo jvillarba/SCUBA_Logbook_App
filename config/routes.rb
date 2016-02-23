154627812241b0a5312624dbd504274118d8bcb6
Joe Villarba <socagambit@aol.com>
2016-02-23 14:24:25 -0800

    working on places

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,6 @@
 Rails.application.routes.draw do
 
-  get '/' => 'users#index'
-  get '/users' => 'users#index', as: :users
-  post '/users' => 'users#create'
-  get '/users/new' => 'users#new', as: :new_user
-  get '/users/:id/edit' => 'users#edit', as: :edit_user
-  get '/users/:id' => 'users#show', as: :user
-  patch '/users/:id' => 'users#update'
-  delete '/users/:id' => 'users#destroy'
+  resources :users
 
   get '/logout' => 'sessions#destroy', as: :logout
   post '/sessions' => 'sessions#create'
